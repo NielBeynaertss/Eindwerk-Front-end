82ce4dc64d64c6e68ca323950e903cae73e69b8e
NBeynaerts <niel.beynaerts@gmail.com>
2023-01-23 15:54:03 +0100

    fixtures code + styling

--- scriptNiel.js
+++ scriptNiel.js
@@ -1,4 +1,4 @@
-
+//Fetch available leagues and display them --------------------------------------------------------------------------------------
 let url = `https://soccer.sportmonks.com/api/v2.0/leagues?api_token=1duNg3kFH9BpL5Y01Zn4PZtQvEQSrU6FVEs5N5LtRduMLdYNFGVIbS1Wg8qA`;
 
 fetch(url)
@@ -10,7 +10,7 @@ fetch(url)
     }
   })
   .then(data => {
-    let container = document.getElementById("leagues-container");
+    let leaguecontainer = document.getElementById("leagues-container");
     data.data.forEach(league => {
       let leagueName = league.name;
       let leagueLogo = league.logo_path;
@@ -23,10 +23,61 @@ fetch(url)
         </div>
       </div>
       `;
-      container.innerHTML += leagueElement;
+      leaguecontainer.innerHTML += leagueElement;
     });
   })
   .catch(error => {
     console.error(error);
   });
 
+
+
+
+//Fetch fixtures and display them -------------------------------------------------------------------------------------------------
+
+  let fixedstartDate = "2022-01-01"
+  let fixedendDate = "2022-01-31"
+  let urlFixtures = `https://soccer.sportmonks.com/api/v2.0/fixtures/between/2022-01-01/2022-01-31?api_token=gesuGMyeJBFXgInr0RYtC94maOJarwO8caWFzbiVEILbmysq6XgTeiEyoqvs`;
+
+  fetch(urlFixtures)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("HTTP error " + response.status);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+
+    let fixturecontainer = document.getElementById("fixtures-container");
+    data.data.forEach(fixture => {
+      let fixture_hometeam_id = fixture.localteam_id;
+      let fixture_awayteam_id = fixture.visitorteam_id;
+      
+      // Make an additional API call to retrieve the name of the home team
+      let urlHomeTeam = `https://soccer.sportmonks.com/api/v2.0/teams/${fixture_hometeam_id}?api_token=StcCBMmkbqygydB9y6ryx0EZQWHxhqYy6qHizL0twlZwlRgdqgbDTgrfihim`;
+      fetch(urlHomeTeam)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          let homeTeamName = data.name;
+          let fixtureElement = 
+          `
+          <b>${homeTeamName}</b>vs<b>${fixture_awayteam_id}</b><br>
+          `;
+          fixturecontainer.innerHTML += fixtureElement;
+        })
+        .catch(error => console.log(error));
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+
+
+
+
+
+  let team = 'https://soccer.sportmonks.com/api/v2.0/teams/{ID}?api_token=1duNg3kFH9BpL5Y01Zn4PZtQvEQSrU6FVEs5N5LtRduMLdYNFGVIbS1Wg8qA'
