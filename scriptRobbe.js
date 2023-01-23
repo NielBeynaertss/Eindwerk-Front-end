31b04cc1b730301538e4a7f860690cefb8144424
RConinx <robbe.coninx@outlook.com>
2023-01-23 10:27:30 +0100

    Add simple buttons for testing

---
+++ scriptRobbe.js
@@ -0,0 +1,13 @@
+document.getElementById("football").addEventListener("click", callFootballAPI);
+
+document.getElementById("travel").addEventListener("click", callTravelAPI);
+
+
+function callFootballAPI() {
+    console.log("FOOTBALL");
+}
+
+
+function callTravelAPI() {
+    console.log("TRAVEL");
+}
