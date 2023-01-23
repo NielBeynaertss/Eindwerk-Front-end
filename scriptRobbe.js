9f1281f033f2f85d166c85f3ddfbec63826bb8ac
RConinx <robbe.coninx@outlook.com>
2023-01-23 10:29:46 +0100

    Linked API's to buttons

--- scriptRobbe.js
+++ scriptRobbe.js
@@ -5,9 +5,27 @@ document.getElementById("travel").addEventListener("click", callTravelAPI);
 
 function callFootballAPI() {
     console.log("FOOTBALL");
+    fetch("https://soccer.sportmonks.com/api/v2.0/fixtures/between/2023-01-02/2023-01-02?api_token=1GoW5Zal0tKjHcvovZTHNVty1B35cuZHol8sz9TPNgwIyl22350MGOEOGdn5")
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
 
 function callTravelAPI() {
     console.log("TRAVEL");
+    var requestOptions = {
+        method: 'GET',
+      };
+      
+    fetch("https://api.geoapify.com/v2/places?categories=catering.restaurant,catering.cafe&filter=circle:11.573106549898483,48.13898913611139,5000&limit=20&apiKey=2e37c02459684f11b9472b5ec244d1e3", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    })
+    .catch(error => console.log('error', error));
 }
