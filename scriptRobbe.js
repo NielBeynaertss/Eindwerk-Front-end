1364c0282866dae782348fe175bd45404de5080a
RConinx <robbe.coninx@outlook.com>
2023-01-23 15:44:16 +0100

    Made api fetch work in multiple scenarios

--- scriptRobbe.js
+++ scriptRobbe.js
@@ -33,23 +33,39 @@ function callTravelAPI() {
     .catch(error => console.log('error', error));
 }
 
-//Make a list of the basic filters for sportmonks api
-//Countries & leagues
-//
-
 
 function testAPIs() {
-    console.log("hello world");
-    fetch("https://soccer.sportmonks.com/api/v2.0/fixtures/between/2023-01-02/2023-01-02?api_token=1GoW5Zal0tKjHcvovZTHNVty1B35cuZHol8sz9TPNgwIyl22350MGOEOGdn5")
+    let team_id = document.getElementById("teams").value
+    console.log(`https://soccer.sportmonks.com/api/v2.0/fixtures/between/2023-02-01/2023-02-28/${team_id}?api_token=1GoW5Zal0tKjHcvovZTHNVty1B35cuZHol8sz9TPNgwIyl22350MGOEOGdn5`)
+    fetch(`https://soccer.sportmonks.com/api/v2.0/fixtures/between/2023-02-01/2023-02-28/${team_id}?api_token=1GoW5Zal0tKjHcvovZTHNVty1B35cuZHol8sz9TPNgwIyl22350MGOEOGdn5`)
     .then(response => response.json())
     .then(data => {
-        const coordinates = data.data[0].weather_report.coordinates;
-        console.log(coordinates);
+        if (data.data[0].weather_report == null) {
+            venue_id = data.data[0].venue_id;
+            console.log(venue_id);
+            return fetch(`https://soccer.sportmonks.com/api/v2.0/venues/${venue_id}?api_token=1GoW5Zal0tKjHcvovZTHNVty1B35cuZHol8sz9TPNgwIyl22350MGOEOGdn5`)
+            .then(response => response.json())
+            .then(data => {
+                coordinates = data.data.coordinates
+                console.log(coordinates)
+                let [lat, lon] = coordinates.split(',');
+                console.log(lon, lat);
+                let geoapifyUrl = `https://api.geoapify.com/v2/places?categories=catering.restaurant,catering.cafe&filter=circle:${lon},${lat},5000&limit=20&apiKey=2e37c02459684f11b9472b5ec244d1e3`
+                console.log(geoapifyUrl)
+                return fetch(geoapifyUrl)
+            }
+    )
+    }
+        else {
+            console.log("DONT GO HERE")
+            const coordinates = data.data[0].weather_report.coordinates;
         const geoapifyUrl = `https://api.geoapify.com/v2/places?categories=catering.restaurant,catering.cafe&filter=circle:${coordinates.lon},${coordinates.lat},5000&limit=20&apiKey=2e37c02459684f11b9472b5ec244d1e3`;
         return fetch(geoapifyUrl);
+    }
     })
     .then(response => response.json())
     .then(data => {
+        console.log("this: " + data)
     // extract the data you need from the response
         let div = document.getElementById("results");
         data.features.forEach((item, index) => {
@@ -70,6 +86,7 @@ function testAPIs() {
 }
 
 function fetchPlaceDetails(url, index) {
+    console.log(url)
     fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -78,9 +95,9 @@ function fetchPlaceDetails(url, index) {
         console.log(placeDetails.contact.phone + " & " + placeDetails.website)
 
         let detailsTemplate = `<p> Phone: ${placeDetails.contact.phone}</p>
-                                <p> Website: ${placeDetails.website}</p>`
-        console.log("hello world")
-        console.log(detailsTemplate)
+                                <p> Website: ${placeDetails.website}</p>
+                                <p> Opening hours: ${placeDetails.opening_hours}</p>
+                                `
         let detailsDiv = document.getElementById(`place-details-${index}`);
         detailsDiv.innerHTML = detailsTemplate;
     })
@@ -89,14 +106,6 @@ function fetchPlaceDetails(url, index) {
     });
 }
 
-
-
-//let extra_info = ""
-//item.properties.categories.forEach(item2 => {
-//    if (item2 == "vegan" || item2 == "vegetarian" || item2 == "halal" || item2 == "wheelchair.yes" ) {
-//        extra_info += item2 + " "
-//    }
-//})
 
 function testOptions() {
     let accOptions = "accommodation." + document.getElementById("accOptions").value;
