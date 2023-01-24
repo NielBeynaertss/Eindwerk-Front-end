4099f49353de636ab761c04283134853a5f6d7cf
NBeynaerts <niel.beynaerts@gmail.com>
2023-01-24 14:05:13 +0100

    Get games based on date + styling

--- scriptNiel.js
+++ scriptNiel.js
@@ -1,5 +1,6 @@
+
 //Fetch available leagues and display them --------------------------------------------------------------------------------------
-let url = `https://soccer.sportmonks.com/api/v2.0/leagues?api_token=1duNg3kFH9BpL5Y01Zn4PZtQvEQSrU6FVEs5N5LtRduMLdYNFGVIbS1Wg8qA`;
+let url = `https://soccer.sportmonks.com/api/v2.0/leagues?api_token=XknJJDTtdX0z1nFtbPxt1C29IestIRI7izPt9gtzTFZP7JDZufu6nAmW8F70`;
 
 fetch(url)
   .then(response => {
@@ -19,7 +20,7 @@ fetch(url)
         <img src="${leagueLogo}" class="card-img-top card_img_style" alt="${leagueName}">
         <div class="card-body text-center">
           <h5 class="card-title">${leagueName}</h5>
-          <a href="#" class="btn btn-primary">Get fixtures</a>
+          <a href="fixturesNiel.html" class="btn btn-primary">Get fixtures</a>
         </div>
       </div>
       `;
@@ -35,49 +36,53 @@ fetch(url)
 
 //Fetch fixtures and display them -------------------------------------------------------------------------------------------------
 
-  let fixedstartDate = "2022-01-01"
-  let fixedendDate = "2022-01-31"
-  let urlFixtures = `https://soccer.sportmonks.com/api/v2.0/fixtures/between/2022-01-01/2022-01-31?api_token=gesuGMyeJBFXgInr0RYtC94maOJarwO8caWFzbiVEILbmysq6XgTeiEyoqvs`;
+let submitButton = document.getElementById("submit-button");
+submitButton.addEventListener("click", function(){
+    let startDate = document.getElementById("start-date").value;
+    let endDate = document.getElementById("end-date").value;
+    let urlFixtures = `https://soccer.sportmonks.com/api/v2.0/fixtures/between/${startDate}/${endDate}?api_token=XknJJDTtdX0z1nFtbPxt1C29IestIRI7izPt9gtzTFZP7JDZufu6nAmW8F70`;
 
-  fetch(urlFixtures)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("HTTP error " + response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-
-    let fixturecontainer = document.getElementById("fixtures-container");
-    data.data.forEach(fixture => {
-      let fixture_hometeam_id = fixture.localteam_id;
-      let fixture_awayteam_id = fixture.visitorteam_id;
-      
-      // Make an additional API call to retrieve the name of the home team
-      let urlHomeTeam = `https://soccer.sportmonks.com/api/v2.0/teams/${fixture_hometeam_id}?api_token=StcCBMmkbqygydB9y6ryx0EZQWHxhqYy6qHizL0twlZwlRgdqgbDTgrfihim`;
-      fetch(urlHomeTeam)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data);
-          let homeTeamName = data.name;
-          let fixtureElement = 
-          `
-          <b>${homeTeamName}</b>vs<b>${fixture_awayteam_id}</b><br>
-          `;
-          fixturecontainer.innerHTML += fixtureElement;
-        })
-        .catch(error => console.log(error));
-    });
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-
-
-
-
-
-
-  let team = 'https://soccer.sportmonks.com/api/v2.0/teams/{ID}?api_token=1duNg3kFH9BpL5Y01Zn4PZtQvEQSrU6FVEs5N5LtRduMLdYNFGVIbS1Wg8qA'
+    fetch(urlFixtures)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      let fixturecontainer = document.getElementById("fixtures-container");
+      data.data.forEach(fixture => {
+        let fixture_hometeam_id = fixture.localteam_id;
+        let urlHomeTeam = `https://soccer.sportmonks.com/api/v2.0/teams/${fixture_hometeam_id}?api_token=XknJJDTtdX0z1nFtbPxt1C29IestIRI7izPt9gtzTFZP7JDZufu6nAmW8F70`;
+        let urlAwayTeam = `https://soccer.sportmonks.com/api/v2.0/teams/${fixture.visitorteam_id}?api_token=XknJJDTtdX0z1nFtbPxt1C29IestIRI7izPt9gtzTFZP7JDZufu6nAmW8F70`;
+        //Promise all
+        Promise.all([fetch(urlHomeTeam),fetch(urlAwayTeam)])
+          .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+          .then(([data1, data2]) => {
+            console.log(data1, data2);
+            let homeTeamName = data1.data.name;
+            let awayTeamName = data2.data.name;
+            let homeTeamLogo = data1.data.logo_path;
+            let awayTeamLogo = data2.data.logo_path;
+            let fixtureElement = 
+            ` 
+                <div class="row fixtures_card">
+                  <div class="col fixtures_cards_col">
+                    <img src="${homeTeamLogo}" class="fixtures_cards_img"></img><br>
+                    <h2 class="text-dark">${homeTeamName}</h2>
+                  </div>
+                  <div class="col fixtures_cards_vs">
+                    <h2 class="text-dark">vs</h2>
+                  </div>
+                  <div class="col fixtures_cards_col">
+                    <img src="${awayTeamLogo}" class="fixtures_cards_img"></img><br>
+                    <h2 class="text-dark">${awayTeamName}</h2>
+                  </div>
+                </div>
+            `;
+            fixturecontainer.innerHTML += fixtureElement;
+          })
+          .catch(error => console.log(error));
+      });
+    })
+});
