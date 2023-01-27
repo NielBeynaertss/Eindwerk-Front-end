68197ab8fddbf3c72545739d7c426b3d320cc10a
NBeynaerts <niel.beynaerts@gmail.com>
2023-01-27 10:54:46 +0100

    header added

--- Eindwerk-frontend-angular/src/app/app.module.ts
+++ Eindwerk-frontend-angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
