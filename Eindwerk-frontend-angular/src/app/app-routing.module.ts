c317ceeb60dd955db079029a56dd2e88e3d08757
NBeynaerts <niel.beynaerts@gmail.com>
2023-01-27 11:25:34 +0100

    paths added

--- Eindwerk-frontend-angular/src/app/app-routing.module.ts
+++ Eindwerk-frontend-angular/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { FaqComponent } from './faq/faq.component';
+import { FixturesComponent } from './fixtures/fixtures.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+    {
+    path: 'fixtures',
+    component: FixturesComponent
   },
   {
     path: 'faq',
