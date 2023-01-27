610003a4eb03a04503f846222e511e226f3d9fd3
NBeynaerts <niel.beynaerts@gmail.com>
2023-01-27 14:29:59 +0100

    index.html completed, show different leagues on load

--- Eindwerk-frontend-angular/src/app/leagues/leagues.component.ts
+++ Eindwerk-frontend-angular/src/app/leagues/leagues.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-leagues',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class LeaguesComponent {
 
+  constructor(private http: HttpClient) {}
+
+  leagues: any;
+
+  ngOnInit() {
+    let url = `https://soccer.sportmonks.com/api/v2.0/leagues?api_token=XknJJDTtdX0z1nFtbPxt1C29IestIRI7izPt9gtzTFZP7JDZufu6nAmW8F70`;
+    this.http.get(url).subscribe(data => {
+      this.leagues = data;
+    });
+  }
+
 }
+
